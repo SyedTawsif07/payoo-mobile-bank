ef40301e3a271bb19f1ac9200e1406b4f1776ddd
SyedTawsif07 <sayedtawsif03@gmail.com>
2026-02-19 03:40:17 +0600

    cashout section created

---
+++ script/cashout.js
@@ -0,0 +1,9 @@
+document.getElementById('click', ()=>{
+    // 1. get the agent number and validate
+    // 2. get the amount, validate, convert to number
+    // 3. get the Current balance, validate, convert to number
+    // 4. calculate new balance
+    // 5. get the pin and verify
+    // 5-1. true:: show an alert > set the balance
+    // 5-2. true:: show an error alert > return 
+})
