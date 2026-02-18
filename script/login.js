bb5beb6ab927d62c3859b04fc4fafb2d38a1ed52
SyedTawsif07 <sayedtawsif03@gmail.com>
2026-02-19 01:59:21 +0600

    login form created and added functionality

--- script/login.js
+++ script/login.js
@@ -1,1 +1,21 @@
-console.log("working")
+document.getElementById('form-btn').addEventListener('click', () => {
+    // 1.get input number
+    const inputNumber = document.getElementById('input-number');
+    const inputValue = inputNumber.value;
+    console.log(inputValue)
+
+    // 2.get pin code
+    const pin = document.getElementById('input-pin');
+    const pinValue = pin.value;
+    console.log(pinValue);
+
+    // 3. Match both number and pin 
+    if(inputValue == '01234567890' && pinValue == '1234'){
+        alert('Successfully loged in')
+        window.location.assign('/home.html')
+    }
+    else{
+        alert('Invalid information')
+        return;
+    }
+})
